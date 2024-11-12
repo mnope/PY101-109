9543bbbc658060789ec5dd9c9e0beec121261806
Mia Kennedy <2tinybirds@gmail.com>
2024-11-12 12:13:02 -0500

    Add new files

---
+++ small_problems/easy/isnt_it_odd.py
@@ -0,0 +1,8 @@
+# Write a function that takes one integer argument 
+def absolute(num):
+    # and returns True when the number's absolute value is odd,
+    if num % 2 == 1:
+        return True
+    # False otherwise. (realizing this part is unnecessary after seeing the solution)
+    else:
+        return False
